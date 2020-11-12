71114d6d7a7d5c003aa4e9a3152a7ef0c08da8b1
Khadhiri786 <60467237+Khadhiri786@users.noreply.github.com>
2020-11-12 17:06:33 +0530

    Add Link and router to Add Photo

--- src/Component/PhotoWall.js
+++ src/Component/PhotoWall.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Photo from './Photo'
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 
 const PhotoWall= (props)=>{
-    return(<div className='photoGrid'>{props.posts.map((post,index)=><Photo post={post} key={index} onRemovePhoto={props.onRemovePhoto}/>)}</div>)
+    return(<> <Link className='addIcon' to='/AddPhoto'>+</Link>
+    <div className='photoGrid'>{props.posts.map((post,index)=><Photo post={post} key={index} onRemovePhoto={props.onRemovePhoto}/>)}</div></>)
 }
 
 PhotoWall.prototype={
