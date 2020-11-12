a24900e5985e0488ac81b98b23dc17701cae3a7c
Khadhiri786 <60467237+Khadhiri786@users.noreply.github.com>
2020-11-12 18:08:50 +0530

    Setup initial project of photowall

--- src/Component/Photo.js
+++ src/Component/Photo.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
-const Photo=(props)=>{
-    const post=props.post;
-        console.log(post.imageLink)
-        return(<figure className="figure">
-            <img  alt={post.description} className='photo' src={post.imageLink}/>
-    <figcaption><p>{post.description}</p></figcaption>
-    <div className='button-container'><button className='remove-button' onClick={()=>{props.onRemovePhoto(post)}}>Remove</button></div>
-    
-        </figure>)
+import PropTypes from 'prop-types'
+function Photo(props) {
+ const post = props.post
+ return <figure className="figure"> 
+ <img className= "photo" src={post.imageLink} alt={post.description} />
+ <figcaption> <p> {post.description} </p> </figcaption>
+ <div className = "button-container">
+ <button onClick = {() => {
+ props.onRemovePhoto(post)
+ }}> Remove </button>
+ </div>
+ </figure>
 }
-
-Photo.prototype={
-    posts:PropTypes.array.isRequired,
-    onRemovePhoto:PropTypes.func.isRequired
+Photo.propTypes = {
+ post: PropTypes.object.isRequired,
+ onRemovePhoto: PropTypes.func.isRequired
 }
-
-export default Photo;
+export default Photo
